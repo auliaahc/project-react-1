c64cb31a3d70dbb3d4ebc3c7dbc2a90f4c010bd7
auhppy <91692012+auhppy@users.noreply.github.com>
2023-05-22 00:52:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
